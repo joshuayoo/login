28ec80496e46baec75e10fa9e6129b447b2fe46a
joshuayoo0120@gmail.com <joshuayoo0120@gmail.com>
2022-08-19 18:35:33 +0900

    HI

---
+++ app.js
@@ -0,0 +1,18 @@
+"use strict"
+
+//module
+const express = require("express");
+const app = express();
+
+//veiw
+app.set("views", "./views");
+app.set("view engine", "ejs");
+
+//router
+const home = require("./routes/home");
+app.use("/", home);
+app.use("/login", home);
+
+//www/app
+
+module.exports = app;
